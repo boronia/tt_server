89537552e21f6d46d8845756d1c62eabb4eed556
Veronica Luke <veronica.luke@gmail.com>
2017-07-11 20:05:50 +1000

    Add barcode search

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -38,10 +38,18 @@ class BarcodesController < ApplicationController
     @barcode.destroy
   end
 
+  def search
+    render json: find_barcode(params[:barcode])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_barcode
       @barcode = Barcode.find(params[:id])
+    end
+
+    def find_barcode(barcode)
+      @barcode = Barcode.find_by_code(barcode)
     end
 
     # Only allow a trusted parameter "white list" through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :barcodes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :barcodes do
+    get 'search', on: :collection
+  end
+
 end
