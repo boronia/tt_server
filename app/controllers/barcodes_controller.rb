2df2629589b490c8eb0033d48a3130e39e0f6fc2
Veronica Luke <veronica.luke@gmail.com>
2017-07-27 13:38:10 +1000

    Search duckduckgo for barcode results

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 class BarcodesController < ApplicationController
   before_action :set_barcode, only: [:show, :update, :destroy]
 
@@ -49,11 +51,34 @@ class BarcodesController < ApplicationController
     end
 
     def find_barcode(barcode)
-      @barcode = Barcode.find_by_code(barcode)
+      #@barcode = Barcode.find_by_code(barcode)
+      search_for_barcode(barcode)
     end
 
     # Only allow a trusted parameter "white list" through.
     def barcode_params
       params.require(:barcode).permit(:code, :description, :manufacturer)
     end
+
+    def search_for_barcode(barcode)
+      uri = "https://duckduckgo.com/html/?q=#{barcode}+product&atb=v32-2b_&ia=web"
+      doc = Nokogiri::HTML(open(uri))
+      results = doc.xpath('//h2/a')
+      barcode_result = nil
+      unless results.empty?
+        node = results[0] # get the first result
+        p_info = extract_packaging_info(results)
+        barcode_result = Barcode.new(:code => barcode, :description => node.text, :packaging_info => p_info)
+        barcode_result.save!
+      end
+      barcode_result
+    end
+
+    def extract_packaging_info(dom)
+      dom.each do
+        # trawl through titles to find keywords
+      end
+      'TODO'
+    end
+
 end
